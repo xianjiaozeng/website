134084e9a5b6ad110986a61ae913809a1d68b7e0
Pinohans <492375757@qq.com>
2016-07-28 23:06:57 +0800

    datastruct完善
    简化了calculateexcel,generateexcel，完成了func1生成，第一个输出，及第二个数据输出，以及在线预览数据的传输，优化了数据结构。

--- datastruct/CalculateExcel.py
+++ datastruct/CalculateExcel.py
@@ -1,39 +1,64 @@
 #coding:utf-8
 from django.conf import settings
+from .models import func1_data1,func1_data2
 import os
 import xlwt
 import xlrd
-from .GenerateExcel import Add,Year,Width,GetFormWidthAndFormLenth
-Score   =   [1,1,1,3,3,5,5]
-def Calculate(username,filename):
-    wb      =   xlwt.Workbook(encoding='utf-8')
-    wbst    =   wb.add_sheet(sheetname='记分卡',cell_overwrite_ok=True)
-    Add(wbst)
-    rb      =   xlrd.open_workbook(os.path.join(os.path.join(settings.MEDIA_ROOT,username),filename))
-    rbst    =   rb.sheet_by_index(sheetx=0)
-    FormWidth,FormLength    =   GetFormWidthAndFormLenth()
+
+def func1_calculate(username,filename):
+    wb_user     =   xlwt.Workbook(encoding='utf-8')
+    wb_user_st1 =   wb_user.add_sheet(sheetname="输出1",cell_overwrite_ok=True)
+    wb_user_st2 =   wb_user.add_sheet(sheetname="输出2",cell_overwrite_ok=True)
+
+    rb          =   xlrd.open_workbook(os.path.join(os.path.join(settings.MEDIA_ROOT,username+'/func1'),filename),on_demand=True)
+    rb_user_st1 =   rb.sheet_by_index(sheetx=0)
+    rb_user_st2 =   rb.sheet_by_index(sheetx=1)
+
+    Score       =   [1,1,1,3,3,5,5]
+
 #    行数i，列数j
-
-    for i in range(1,rbst.nrows,Width):
+    Effectiveness   =   [0,0]
+    for i,a in enumerate(func1_data1.objects.all().order_by('ItemName','SubItemName')):
         LatestScore =   0
-        for j in range(FormLength,rbst.ncols):
-            targ    =   rbst.cell(i,j)
-            real    =   rbst.cell(i+1,j)
-            wbst.write(i,j,targ.value)
-            wbst.write(i+1,j,real.value)
+        TmpScore    =   0
+        for j in range(5,rb_user_st1.ncols):
+            targ    =   rb_user_st1.cell(i*2+1,j)
+            real    =   rb_user_st1.cell(i*2+2,j)
             if targ.ctype == 2 and real.ctype == 2:
-                if targ.value != 0:
-                    wbst.write(i+2,j,(real.value-targ.value)/targ.value,style=xlwt.Style.easyxf(num_format_str="0.00%"))
-                    TmpCnt  =   0
-                    for k in range(6):
-                        if j - k < FormLength:
-                            break
-                        if rbst.cell(i,j-k).value<=rbst.cell(i+1,j-k).value:
-                            TmpCnt+=1
-                    wbst.write(i+3,j,TmpCnt)
-                    LatestScore=TmpCnt
-        wbst.write_merge(i,i+Width-1,3,3,Score[LatestScore])
+                if targ.value<=real.value:
+                    TmpScore = ((TmpScore << 1) & 0b111111) | 1
+                else:
+                    TmpScore = ((TmpScore << 1) & 0b111111) | 0
+        while TmpScore != 0:
+            LatestScore = LatestScore + (TmpScore & 1)
+            TmpScore    = TmpScore >> 1
+
+        Effectiveness[0] = Effectiveness[0] + a.Weight * Score[LatestScore]
+        Effectiveness[1] = Effectiveness[1] + a.Weight * a.MaxSingle
 
 
-#    wbst.write(4,8,xlwt.Formula("A1"))
-    wb.save(os.path.join(os.path.join(os.path.join(settings.MEDIA_ROOT,username),'result'),filename))
+    Title       =   ["评估项", "评估结果"]
+    Maturity    =   []
+    TotalMaturity   =   ["评估总分",0,0]
+    for i,a in enumerate(Title):
+        wb_user_st1.write(i,0,a)
+
+    for i,a in enumerate(func1_data2.objects.all().order_by('ItemName','Serial')):
+        if len(Maturity)==0 or Maturity[-1][0]!=a.ItemName:
+            Maturity.append([a.ItemName,0,0])
+        Maturity[-1][1] = Maturity[-1][1] + a.Weight * rb_user_st2.cell(i+1,3).value
+        Maturity[-1][2] = Maturity[-1][2] + a.Weight * a.MaxSingle
+    for i,a in enumerate(Maturity):
+        wb_user_st1.write(i+1,0,a[0])
+        wb_user_st1.write(i+1,1,a[1]/float(a[2]))
+        TotalMaturity[1]=TotalMaturity[1]+a[1]
+        TotalMaturity[2]=TotalMaturity[2]+a[2]
+    wb_user_st1.write(len(Maturity)+1,0,TotalMaturity[0])
+    wb_user_st1.write(len(Maturity)+1,1,TotalMaturity[1]/float(TotalMaturity[2]))
+
+    wb_user_st2.write(0,0,'Effectiveness')
+    wb_user_st2.write(1,0,Effectiveness[0]/float(Effectiveness[1]))
+    wb_user_st2.write(0,1,'Maturity')
+    wb_user_st2.write(1,1,TotalMaturity[1]/float(TotalMaturity[2]))
+
+    wb_user.save(os.path.join(os.path.join(settings.MEDIA_ROOT,username+'/func1'),'result.xls'))
--- datastruct/GenerateExcel.py
+++ datastruct/GenerateExcel.py
@@ -1,83 +1,44 @@
 #coding:utf-8
 import xlwt
 from django.conf import settings
+from .models import func1_data1,func1_data2
 import os
-TitleName   =   ["评分项", "评分子项", "计量单位","评分标准（1/3/5）", "权重(1/3/5)", "单项最高分", "最高总分", "内容"]
-Item        =   []
-Detail      =   ["目标", "实际结果", "差率", "最近6个月内满足要求的月份数"]
-Month       =   ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
-Year        =   3
-Width       =   len(Detail)
-Length      =   len(TitleName)
 
 
-def GetFormWidthAndFormLenth():
-    global Item
-    FormWidth   =   0
-    FormLength  =   0
-    for i in range(len(Item[0])):
-        FormWidth+=len(Item[1][i])*Width
-    FormWidth+=1
-    FormLength=Length
-    return FormWidth,FormLength
+#第一个功能
+#
+def func1_create():
+    wb_user     =   xlwt.Workbook(encoding='utf-8')
+    wb_user_st1 =   wb_user.add_sheet(sheetname="记分卡评估",cell_overwrite_ok=True)
+    wb_user_st2 =   wb_user.add_sheet(sheetname="成熟度评估",cell_overwrite_ok=True)
 
-def Add(st):
-    global TitleName,Item,Detail,Month,Year,Width,Length
-    ItemSt  =   0
-    ItemEd  =   1
+
+    Title       =   ["评分项", "评分子项", "计量单位", "内容"]
+    Detail      =   ["目标", "实际结果"]
+    Month       =   ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
+    Year        =   3
 
 #    题头
-    for i in range(Length):
-        st.write_merge(ItemSt,ItemEd-1,i,i,TitleName[i])
-    for i in range(12*Year):
-        st.write_merge(ItemSt,ItemEd-1,i+Length,i+Length,Month[i%12])
+    for i,a in enumerate(Title+Month*Year):
+        wb_user_st1.write(0,i,a)
 
 #    内容
-    for i in range(len(Item[0])):
-        ItemSt      =   ItemEd
-        ItemEd      =   ItemSt + Width * len(Item[1][i])
-        st.write_merge(ItemSt,ItemEd-1,0,0,Item[0][i])
-        for j in range(len(Item[1][i])):
-            for k in range(1,6):
-                if k <3:
-                    st.write_merge(ItemSt + j * Width,ItemSt + (j + 1) * Width-1,k,k,Item[k][i][j])
-                else:
-                    st.write_merge(ItemSt + j * Width,ItemSt + (j + 1) * Width-1,k+1,k+1,Item[k][i][j])
-            for k in range(Width):
-                st.write_merge(ItemSt + j * Width + k,ItemSt + j * Width + k,7,7,Detail[k])
+    for i,a in enumerate(func1_data1.objects.all().order_by('ItemName','SubItemName')):
+        wb_user_st1.write_merge(i*2+1,i*2+2,0,0,a.ItemName)
+        wb_user_st1.write_merge(i*2+1,i*2+2,1,1,a.SubItemName)
+        wb_user_st1.write_merge(i*2+1,i*2+2,2,2,a.UnitName)
+        for j,b in enumerate(Detail):
+            wb_user_st1.write(i*2+j+1,3,b)
 
+    Title       =   ["评估项", "序列号", "评估子项","评估内容及打分标准"]
 
+#    题头
+    for i,a in enumerate(Title):
+        wb_user_st2.write(0,i,a)
+#    内容
+    for i,a in enumerate(func1_data2.objects.all().order_by('ItemName','Serial')):
+        wb_user_st2.write(i+1,0,a.ItemName)
+        wb_user_st2.write(i+1,1,a.Serial)
+        wb_user_st2.write(i+1,2,a.SubItemName)
 
-def Create(queryset):
-    global TitleName,Item,Detail,Month,Year,Width,Length,FormWidth,FormLength
-
-    wb      =   xlwt.Workbook(encoding='utf-8')
-    wbst    =   wb.add_sheet(sheetname="记分卡",cell_overwrite_ok=True)
-    Item    =   []
-    for a in queryset:
-        if len(Item)==0:
-            Item.append([a.ItemName])
-            Item.append([[a.SubItemName]])
-            Item.append([[a.UnitName]])
-            Item.append([[a.Weight]])
-            Item.append([[a.MaxSingle]])
-            Item.append([[a.Weight*a.MaxSingle]])
-        elif a.ItemName not in Item[0]:
-            Item[0].append(a.ItemName)
-            Item[1].append([a.SubItemName])
-            Item[2].append([a.UnitName])
-            Item[3].append([a.Weight])
-            Item[4].append([a.MaxSingle])
-            Item[5].append([a.Weight*a.MaxSingle])
-        else:
-            pos=Item[0].index(a.ItemName)
-            Item[1][pos].append(a.SubItemName)
-            Item[2][pos].append(a.UnitName)
-            Item[3][pos].append(a.Weight)
-            Item[4][pos].append(a.MaxSingle)
-            Item[5][pos].append(a.Weight*a.MaxSingle)
-
-    Add(wbst)
-    wb.save(os.path.join(os.path.join(settings.MEDIA_ROOT,'common'),'a.xls'))
-
-    pass
+    wb_user.save(os.path.join(os.path.join(settings.MEDIA_ROOT,'common/func1'),'user.xls'))
